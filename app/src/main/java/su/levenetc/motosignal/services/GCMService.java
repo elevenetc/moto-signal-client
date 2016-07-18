9f04d5795f9c9932a9207d10b81cec25f066d210
Eugene Levenetc <shumvlesu@gmail.com>
2016-07-18 23:47:47 +0200

    Add GCM implementation

---
+++ app/src/main/java/su/levenetc/motosignal/services/GCMService.java
@@ -0,0 +1,14 @@
+package su.levenetc.motosignal.services;
+
+import android.os.Bundle;
+
+import com.google.android.gms.gcm.GcmListenerService;
+
+/**
+ * Created by Eugene Levenetc on 18/07/2016.
+ */
+public class GCMService extends GcmListenerService {
+	@Override public void onMessageReceived(String s, Bundle bundle) {
+		super.onMessageReceived(s, bundle);
+	}
+}

53132c6268d7f595ca5675afbe257d8e6078fd7e
Eugene Levenetc <shumvlesu@gmail.com>
2016-07-19 23:57:42 +0200

    Clean up

--- app/src/main/java/su/levenetc/motosignal/activities/SettingsActivity.java
+++ app/src/main/java/su/levenetc/motosignal/activities/SettingsActivity.java
@@ -1,8 +1,11 @@
 package su.levenetc.motosignal.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import su.levenetc.motosignal.services.GCMRegistrationService;
 
 /**
  * Created by Eugene Levenetc on 17/07/2016.
@@ -10,5 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 public class SettingsActivity extends AppCompatActivity {
 	@Override protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		startService(new Intent(this, GCMRegistrationService.class));
 	}
 }
--- app/src/main/java/su/levenetc/motosignal/services/InstanceIDService.java
+++ app/src/main/java/su/levenetc/motosignal/services/InstanceIDService.java
@@ -9,19 +9,9 @@ import com.google.android.gms.iid.InstanceIDListenerService;
  */
 public class InstanceIDService extends InstanceIDListenerService {
 
-	private static final String TAG = "MyInstanceIDLS";
-
-	/**
-	 * Called if InstanceID token is updated. This may occur if the security of
-	 * the previous token had been compromised. This call is initiated by the
-	 * InstanceID provider.
-	 */
-	// [START refresh_token]
 	@Override
 	public void onTokenRefresh() {
-		// Fetch updated Instance ID token and notify our app's server of any changes (if applicable).
-		Intent intent = new Intent(this, RegistrationIntentService.class);
+		Intent intent = new Intent(this, GCMRegistrationService.class);
 		startService(intent);
 	}
-	// [END refresh_token]
 }
